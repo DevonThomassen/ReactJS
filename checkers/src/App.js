460a12d7f4848f9b7fc9152cc27d373593478d65
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-10-18 12:42:14 +0200

    Update App.js

--- checkers/src/App.js
+++ checkers/src/App.js
@@ -1,6 +1,38 @@
 import React, {Component} from 'react';
 import './App.css';
-import Soldier from "./components/board";
+
+function Pieces(props) {
+    const pieceLocation = props.location;
+    let a = <td key={"td" + pieceLocation}>{pieceLocation}</td>;
+    let y = pieceLocation.charAt(0);
+    let z = pieceLocation.charAt(1);
+    let even = isEven(z);
+    let black = blackLocation(z);
+    let white = whiteLocation(z);
+
+    if ((even === true && black === true) || (even === true && white === true)) {
+        if (y === 'A' || y === 'C' || y === 'E' || y === 'G') {
+            return a = <td><span key={"span" + pieceLocation} className={black ? "soldier black" : "soldier white"}>{pieceLocation}</span></td>;
+        }
+    }
+    else if ((even === false && black === true) || (even === false && white === true)) {
+        if (y === 'B' || y === 'D' || y === 'F' || y === 'H') {
+            return a = <td key={"span" + pieceLocation}><span className={black ? "soldier black" : "soldier white"}>{pieceLocation}</span></td>;
+        }
+    }
+    return a;
+}
+
+// Math
+function isEven(x) {
+    return parseInt(x) % 2 === 0;
+}
+function blackLocation(x) {
+    return parseInt(x) === 8 || parseInt(x) === 7 || parseInt(x) === 6;
+}
+function whiteLocation(x) {
+    return parseInt(x) === 3 || parseInt(x) === 2 || parseInt(x) === 1;
+}
 
 class App extends Component {
     state = {
@@ -12,11 +44,9 @@ class App extends Component {
         return (
             <table>
                 {this.state.v.map(rowV => (
-                    <tr>
+                    <tr key={"row" + rowV}>
                         {this.state.h.map(rowH => (
-                            <td key={rowH + rowV}
-                                id={rowH + rowV}>{rowH + rowV}</td>
-                        ))}
+                            <Pieces location={rowH + rowV}/>))}
                     </tr>
                 ))}
             </table>
