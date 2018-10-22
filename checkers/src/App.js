f370534d8029cf74153fc768f36dd8727eb88a5a
Devon Thomassen <39370515+DevonThomassen@users.noreply.github.com>
2018-10-22 22:51:50 +0200

    Update App.js

--- checkers/src/App.js
+++ checkers/src/App.js
@@ -2,63 +2,65 @@ import React, {Component} from 'react';
 import './App.css';
 
 function Pieces(props) {
-    const pieceLocation = props.location;
-    let a = <td key={"td" + pieceLocation}>{pieceLocation}</td>;
-    let y = pieceLocation.charAt(0);
-    let z = pieceLocation.charAt(1);
-    let even = isEven(z);
-    let black = blackLocation(z);
-    let white = whiteLocation(z);
+  const pieceLocation = props.location;
+  let a = <td key={"td" + pieceLocation}>{pieceLocation}</td>;
+  let y = pieceLocation.charAt(0);
+  let z = pieceLocation.charAt(1);
+  let even = isEven(z);
+  let black = blackLocation(z);
+  let white = whiteLocation(z);
 
-    if (((even === true && black === true) || (even === true && white === true)) &&
-        (y === 'A' || y === 'C' || y === 'E' || y === 'G')) {
-        return a =
-            <td key={"td" + pieceLocation}>
-                <span key={"span" + pieceLocation}
-                      className={black ? "soldier black" : "soldier white"}>{pieceLocation}
-                </span>
-            </td>;
-    }
-    else if (((even === false && black === true) || (even === false && white === true)) &&
-        (y === 'B' || y === 'D' || y === 'F' || y === 'H')) {
-        return a =
-            <td key={"td" + pieceLocation}>
+  if (((even === true && black === true) || (even === true && white === true)) &&
+    (y === 'A' || y === 'C' || y === 'E' || y === 'G')) {
+    return a =
+      <td key={"td" + pieceLocation}>
+        <span key={"span" + pieceLocation}
+              className={black ? "soldier black" : "soldier white"}>{pieceLocation}
+        </span>
+      </td>;
+  }
+  else if (((even === false && black === true) || (even === false && white === true)) &&
+    (y === 'B' || y === 'D' || y === 'F' || y === 'H')) {
+    return a =
+      <td key={"td" + pieceLocation}>
                 <span key={"span " + pieceLocation}
                       className={black ? "soldier black" : "soldier white"}>{pieceLocation}</span>
-            </td>;
-    }
-    return a;
+      </td>;
+  }
+  return a;
 }
 
 // Math
 function isEven(x) {
-    return parseInt(x) % 2 === 0;
+  return parseInt(x) % 2 === 0;
 }
+
 function blackLocation(x) {
-    return parseInt(x) === 8 || parseInt(x) === 7 || parseInt(x) === 6;
+  return parseInt(x) === 8 || parseInt(x) === 7 || parseInt(x) === 6;
 }
+
 function whiteLocation(x) {
-    return parseInt(x) === 3 || parseInt(x) === 2 || parseInt(x) === 1;
+  return parseInt(x) === 3 || parseInt(x) === 2 || parseInt(x) === 1;
 }
 
 class App extends Component {
-    state = {
-        v: [8, 7, 6, 5, 4, 3, 2, 1],
-        h: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
-    };
+  state = {
+    v: [8, 7, 6, 5, 4, 3, 2, 1],
+    h: ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+  };
 
-    render() {
-        return (
-            <table>
-                {this.state.v.map(rowV => (
-                    <tr key={"row" + rowV}>
-                        {this.state.h.map(rowH => (
-                            <Pieces key={rowH + rowV} location={rowH + rowV}/>))}
-                    </tr>
-                ))}
-            </table>
-        );
-    }
+  render() {
+    return (
+      <table>
+        {this.state.v.map(rowV => (
+          <tr key={"row" + rowV}>
+            {this.state.h.map(rowH => (
+              <Pieces key={rowH + rowV} location={rowH + rowV}/>))}
+          </tr>
+        ))}
+      </table>
+    );
+  }
 }
 
 export default App;
